615d9a5abceb01cec0c02db27a1fc84ced576119
sgill1954 <ssgill@iu.edu>
2026-02-11 21:14:56 -0500

    Update models.py with patient model
    I created patients model based attributes from patient.csv. Will need to add some more functions later.

--- qubits_capstone/website/models.py
+++ qubits_capstone/website/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    patient_id = models.AutoField(primary_key=True)
+    
+    # Personal Information
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=20)
+    
+    # Contact Details
+    contact_number = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+    
+    # Administrative Data
+    registration_date = models.DateTimeField(auto_now_add=True)
+    insurance_provider = models.CharField(max_length=150)
+    insurance_number = models.CharField(max_length=50)
+
+     # 1. readable name of the patient
+    def get_full_name(self):
+        """Returns the patient's full name."""
+        return f"{self.first_name} {self.last_name}"
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
