620b516e838b67a23ad9bedbb459d604e4277da4
m-priestly <m.priestly@outlook.com>
2026-02-18 19:20:09 -0500

    Adjusted staff model and created vital model

--- qubits_capstone/website/models.py
+++ qubits_capstone/website/models.py
@@ -8,6 +8,28 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 #meta allows you to configure table level behavior
 #str___self determines how it is displayed
 
+#------------------------STAFF Information______________________________
+
+class Staff(models.Model):
+    # opted out of years of experience, unnecessary 
+    # staff general information 
+    staff_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    #title
+    speacialization = models.CharField(max_length = 50)
+    primary_branch = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)  
+    email = models.CharField(max_length= 100, null= True, blank = True)
+    # the current csv file only contains Dr.'s, the staff that will use this tool the most will be nurses
+    title = models.CharField(max_length=50, null=True , blank= True)
+
+    def __str__(self):
+         return f"{self.first_name[0]},{self.last_name}, {self.title}"
+
+  
+
 
 #-----------------------------------Patient Information----------------------------
 
@@ -16,20 +38,17 @@ class Patient(models.Model):
     patient_id = models.AutoField(primary_key=True) # would we want this to be connected to an MRN?
     
     # Personal Information
+    #attributes deleted = contact_number, email, address, insurance_provider, insurance_number
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=20)
+    gender = models.CharField(max_length=20) #our output should say sex, not gender
     
-    # Contact Details
-    contact_number = models.CharField(max_length=20)    #These things would be included in the EMR? 
-    email = models.EmailField(unique=True)
-    address = models.TextField()
     
     # Administrative Data
     registration_date = models.DateTimeField(auto_now_add=True)
-    insurance_provider = models.CharField(max_length=150)  #do we need to know patient provider?
-    insurance_number = models.CharField(max_length=50)  #do we need to know the insurance?
+   
 
      # 1. readable name of the patient
     def get_full_name(self):
@@ -41,7 +60,7 @@ class Patient(models.Model):
     doctor = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank= True,  
                                 limit_choices_to={"title__in": ["Doctor","MD","DO"]},
                                 related_name= "doctor_patients")
-    nurse = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank = True, # should we remove this would there not have to be a care provider for them to be in the ER? 
+    nurse = models.ForeignKey(Staff, on_delete=models.SET_NULL, 
                                 limit_choices_to={"title__in": ["Nurse", "RN"]},
                                                   related_name= "nurse_patients")
     
@@ -54,6 +73,8 @@ class Patient(models.Model):
 
     def __str__(self):
         return f"{self.get_full_name()}\n\tAssigned Dr: {self.doctor}\n\tAssigned Nurse: {self.nurse}"   
+
+#-----------------------------------Vitals---------------------------------------
 
 
 #------------------------------------Visit Model-----------------------------
@@ -167,25 +188,31 @@ class Triage_scores(models.Model):
     def __str__(self):
         return f"ESI Level {self.esi_level} assigned by {self.assigned_by}
 
-#------------------------STAFF Information______________________________
 
-class Staff(models.Model):
 
-    # staff general information 
-    staff_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+#-----------------------------Vitals----------------------------------------
 
-    #title
-    department = models.CharField(max_length = 50)
-    primary_Hosptial = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)  
-    ext = models.CharField(max_length=100, null= True, blank = True)
-    title = models.CharField(max_length=50, null=True , blank= True)
+class Vitals(models.Model):
+    heart_rate = models.IntegerField()
+    systolic_blood_pressure = models.IntegerField()
+    oxygen_saturation= models.IntegerField()
+    body_temperature= models.DecimalField(max_digits=4, decimal_places=1)
+    pain_level=models.IntegerField()
+    chronic_disease_count = models.IntegerField()
+
+    time_of_vitals = models.DateTimeField(auto_now_add= True)
 
     def __str__(self):
-         return f"{self.first_name[0]},{self.last_name}, {self.title}"
-   
+        return f" Vitals were recorded at {self.time_of_vitals} \n\t HR: {self.heart_rate} \n\t Systolic BP: {self.systolic_blood_pressure} \n\t Pulse Ox:{self.oxygen_saturation} \n\t Body Temp:{self.body_temperature} \n\t Reported Painlevel:{self.pain_level}"
+
+    class Meta:
+        ordering = ['time_of_vitals']
+        verbose_name_plural = "Vitals"
+
+
+
+
+
     
 
 
