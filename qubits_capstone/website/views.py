573bb4fd39b3f471cba0ebbc51cff07d186e8366
Bailey Crumpler <baileystarrc4@gmail.com>
2026-03-15 11:32:12 -0400

    view method to do

--- qubits_capstone/website/views.py
+++ qubits_capstone/website/views.py
@@ -10,3 +10,11 @@ def in_building(request):
     all_visits= website.models.Visit.objects.all()
     context = {'all_visits': all_visits}
     return render(request,"in_building.html", context)
+
+## example of creating objects using model is in csv script
+## TODO create new_intake view function
+    # if the request a post request (submit button was clicked)
+        # create new patient object
+        # create new vitals object
+    # else (request is a get request)
+        # return page render 
