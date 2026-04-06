b1f5c6352561c66da5fd6cefeaadc5f1bc72f045
Bailey Crumpler <baileystarrc4@gmail.com>
2026-04-06 00:30:20 -0400

    Update views.py
    saves complaint in visit, handles esi override, and creates the assessment and score objects. also clears session data.

--- qubits_capstone/website/views.py
+++ qubits_capstone/website/views.py
@@ -3,13 +3,14 @@ from django.contrib.auth.decorators import login_required
 import website.models
 from .forms import PatientForm, VitalsForm, HighRiskForm, PatientLeftForm
 from django.contrib import messages
+# from .webapp_scripts.esi_logic import get_esi_for_vital_id
 
 
 @login_required
 def home(request):
     return render(request, "home.html")
 
-
+@login_required
 def patient_intake(request):
     all_visits = website.models.Visit.objects.all()
 
@@ -31,12 +32,20 @@ def patient_intake(request):
         elif "high_risk_submit" in request.POST:
             high_risk_form = HighRiskForm(request.POST)
             if high_risk_form.is_valid():
+
+                # get visit and save the complaint to it
+                visit_primary_key = request.session.get("current_visit_id")
+                visit = website.models.Visit.objects.get(visit_id=visit_primary_key)
+                visit.complaint = high_risk_form.cleaned_data["complaint"]
+                visit.save()
+
                 # save in session to be used when vital is created
                 request.session['life_saving_intervention'] = high_risk_form.cleaned_data["life_saving_intervention"]
                 request.session['high_risk'] = high_risk_form.cleaned_data["high_risk"]
                 request.session['disoriented'] = high_risk_form.cleaned_data["disoriented"]
                 request.session['severe_pain'] = high_risk_form.cleaned_data["severe_pain"]
                 request.session['diff_resources'] = high_risk_form.cleaned_data["diff_resources"]
+
             else:
                 print(high_risk_form.errors)
         
@@ -57,8 +66,28 @@ def patient_intake(request):
                 curr_vitals.severe_pain = request.session.get("severe_pain")
                 curr_vitals.diff_resources = request.session.get("diff_resources")
 
-                # commit everything to db
+                # commit vitals to db
                 curr_vitals.save()
+
+                # get the score if they manually entered it
+                esi_score = vitals_form.cleaned_data.get("esi_override")
+
+                # # if no override, calculate using script
+                # if not esi_score:
+                #     esi_score = get_esi_for_vital_id(curr_vitals.Vitals_id)
+
+                # # after obtaining level, save assessment and score
+                # curr_assessment = website.models.TriageAssessment.objects.create(vitals_id = curr_vitals, visit_id = curr_visit)
+                # website.models.Triage_scores.objects.create(triage_id=curr_assessment, esi_level = esi_score)
+
+                # after everything has saved, remove saved session values to avoid errors in later entries
+                request.session.pop('current_visit_id', None)    
+                request.session.pop('life_saving_intervention', None) 
+                request.session.pop('high_risk', None) 
+                request.session.pop('disoriented', None) 
+                request.session.pop('severe_pain', None) 
+                request.session.pop('diff_resources', None) 
+
             else:
                 print(vitals_form.errors)
 
