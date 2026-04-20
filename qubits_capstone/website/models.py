33610a386cffc1d0bae15dc5079ae83c6661aad8
Bailey Crumpler <baileystarrc4@gmail.com>
2026-04-20 00:05:18 -0400

    cleaning up models
    deleted fields from models that are no longer used (complaints, queue, doctor stuff, repeated code, etc)

--- qubits_capstone/website/models.py
+++ qubits_capstone/website/models.py
@@ -4,9 +4,6 @@ from django.db.models.base import ModelState
 from django.utils import timezone
 from datetime import timedelta
 from django.core.validators import MaxValueValidator, MinValueValidator
-#Foriegnkey links two models together 
-#meta allows you to configure table level behavior
-#str___self determines how it is displayed
 
 #------------------------Shift Information------------------------
 class Shift(models.Model):
@@ -25,8 +22,6 @@ class Shift(models.Model):
 #------------------------STAFF Information------------------------
 
 class Staff(models.Model):
-    # opted out of years of experience, unnecessary 
-    # staff general information 
     staff_id = models.AutoField(primary_key=True)
     
     shift_id = models.ForeignKey(
@@ -34,80 +29,42 @@ class Staff(models.Model):
         related_name='shift',
         db_column='shift_id', 
         on_delete=models.PROTECT, 
-        default=2
+        default=1
     )
-
-    # (..., null = True, blank = True) allows for those variables to be left null/empty 
 
     first_name = models.CharField(max_length=100)
 
     last_name = models.CharField(max_length=100)
 
-    #title
-    specialization = models.CharField(max_length = 50)
-
-    primary_branch = models.CharField(max_length=100)
-
     phone_number = models.CharField(max_length=15)  
 
     email = models.CharField(max_length= 100, null= True, blank = True)
-
-    # the current csv file only contains Dr.'s, the staff that will use this tool the most will be nurses
-    title = models.CharField(max_length=50, null=True , blank= True)
-
    
-
     def __str__(self):
          return f"{self.first_name[0]},{self.last_name}, {self.title}"
-
-  
-
 
 #-----------------------------------Patient Information----------------------------
 
 #Patient Data 
 class Patient(models.Model):
     patient_id = models.AutoField(primary_key=True) # would we want this to be connected to an MRN?
-    
-    # Personal Information
-    #attributes deleted = contact_number, email, address, insurance_provider, insurance_number
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True,  blank=True)
     gender = models.CharField(max_length=6, choices=(('male','Male'),('female', 'Female'),('other','Other')), null=True, blank=True) #our output should say sex, not gender
     
-    
-     # 1. readable name of the patient
     def get_full_name(self):
         """Returns the patient's full name."""
         return f"{self.first_name} {self.last_name}"
 
-
-  # Care Team **** ADDED THIS****
-    doctor = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank= True,  
-                                limit_choices_to={"title__in": ["Doctor","MD","DO"]},
-                                related_name= "doctor_patients")
     nurse = models.ForeignKey(Staff, 
                               on_delete=models.SET_NULL, 
-                              limit_choices_to={"title__in": ["Nurse", "RN"]},
                               related_name= "nurse_patients", 
-                              #from Bailey: had to add for csv import to work. can delete later
                               null=True,
                               blank=True)
-    
-
-
-  #Should we add final traige score?
-
-    def get_full_name(self):
-     return f"{self.first_name} {self.last_name}"
-
     def __str__(self):
         return f"{self.get_full_name()}\n\tAssigned Dr: {self.doctor}\n\tAssigned Nurse: {self.nurse}"   
-
-
-
 
 #------------------------------------Visit Model-----------------------------
 class Visit(models.Model):
@@ -121,42 +78,10 @@ class Visit(models.Model):
         related_name='visits',
         db_column='patient_id'
     )
-
-    complaint = models.CharField(blank = True, null = True)
     
     arrival_time = models.DateTimeField(default=timezone.now)
     exiting_time = models.DateTimeField(null=True, blank=True)
-        
-    # These fields will now be calculated automatically
-    queue_count_before_processing = models.IntegerField(default=0, editable=False)
-    queue_count_after_processing = models.IntegerField(default=0, editable=False)
-    queue_difference = models.IntegerField(null=True, editable=False)
-
-    def save(self, *args, **kwargs):
-        # 1. Calculate Queue BEFORE (Patients who arrived but haven't exited yet)
-        if not self.visit_id:  # Only calculate on first creation
-            self.queue_count_before_processing = Visit.objects.filter(
-                arrival_time__lt=self.arrival_time,
-                exiting_time__isnull=True
-            ).count()
-
-        # 2. Logic when the patient exits
-        if self.exiting_time:
-            # Calculate Queue AFTER (Others still waiting when this person leaves)
-            self.queue_count_after_processing = Visit.objects.filter(
-                arrival_time__lt=self.exiting_time,
-                exiting_time__isnull=True
-            ).exclude(pk=self.visit_id).count()
             
-            # Calculate Waiting Time
-            if self.arrival_time:
-                self.waiting_time = self.exiting_time - self.arrival_time
-        
-        # 3. Calculate the Difference
-        # Logic: If 10 were there before, and 4 are there after, difference is 6
-        self.queue_difference = self.queue_count_before_processing - self.queue_count_after_processing
-            
-        super(Visit, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"Visit {self.visit_id} (Queue Before: {self.queue_count_before_processing})"
